6f5985d804e1f445c8cf309e1ce0eb692150333c
Alexandra Seligová <alexaseligova@gmail.cz>
2023-10-31 22:47:32 +0100

    + přidání gui souborů do projektu, - odebrání původních prvků ( budou jiné) + přidání druhů stanic

---
+++ GUI/GuiStation/Components/GSMainButtons.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoS.GUI.GuiStation.Components
+{
+    internal class GSMainButtons
+    {
+    }
+}
