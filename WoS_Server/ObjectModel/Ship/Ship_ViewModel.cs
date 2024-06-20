e282793b2f92dba10cce70d9b14535ae7731bba1
Alexandra Seligová <alexaseligova@gmail.com>
2024-06-20 07:14:26 +0200

    *

---
+++ WoS_Server/ObjectModel/Ship/Ship_ViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WoS_Server.ObjectModel.Ship
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Ship_ViewModel
+    {
+    }
+}
